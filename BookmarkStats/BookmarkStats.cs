0ce045560e23669ff2888ee896df436faf73ec10
foxneZz <foxnezz@gmail.com>
2017-03-17 23:48:38 +0300

    added date related stats

--- BookmarkStats/BookmarkStats.cs
+++ BookmarkStats/BookmarkStats.cs
@@ -17,9 +17,34 @@ namespace BookmarkStats
         {
             var contents = "";
 
-            contents += $"Bookmark file fetch date: {folder.DateAdded}\n\n";
+            contents += $"Bookmark file fetch date: {folder.DateAdded}\n";
 
             var bookmarks = (List<Bookmark>)folder;
+
+            contents += GetDateExtremes(bookmarks);
+            contents += GetMostBookmarkedWebsites(bookmarks);
+
+            return contents;
+        }
+
+        private static string GetDateExtremes(List<Bookmark> bookmarks)
+        {
+            var contents = "\n";
+
+            var ordered = bookmarks.OrderBy(pair => pair.DateAdded);
+            var first = ordered.First().DateAdded;
+            var last = ordered.Last().DateAdded;
+            contents += $"The first bookmark was added at {first}\n";
+            contents += $"The last bookmark was added at {last}\n";
+            contents += $"Time span in between the these dates: {Readable(last - first)}\n";
+
+            return contents;
+        }
+
+        private static string GetMostBookmarkedWebsites(List<Bookmark> bookmarks)
+        {
+            var contents = "\n";
+
             var websites = new Dictionary<string, int>();
             int nonWebsiteCount = 0;
             foreach (var bookmark in bookmarks)
@@ -45,7 +70,31 @@ namespace BookmarkStats
             for (int i = 0; i < orderedWebsites.Count; ++i)
                 contents += $"{i + 1}. {orderedWebsites[i].Key} : {orderedWebsites[i].Value} hits\n";
 
-            return contents;
+            return contents + "\n";
+        }
+
+        private static string Readable(TimeSpan ts)
+        {
+            const double daysInYear = 365.242199;
+            const double daysInMonth = daysInYear / 12;
+
+            var totalDays = ts.TotalDays;
+            int years = (int)(totalDays / daysInYear);
+            totalDays -= years * daysInYear;
+            int months = (int)(totalDays / daysInMonth);
+            totalDays -= months * daysInMonth;
+            int days = (int)totalDays;
+            
+            var contents = "";
+            if (years > 0)
+                contents += $"{years} year{(years != 1 ? "s" : "")} ";
+
+            if (months > 0)
+                contents += $"{months} month{(months != 1 ? "s" : "")} ";
+
+            contents += $"{days} day{(days != 1 ? "s" : "")}";
+
+            return contents.TrimEnd();
         }
     }
 }
