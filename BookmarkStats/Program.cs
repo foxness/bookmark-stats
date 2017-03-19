678a8472c908b87faab2faa62250bd4b2803f8e4
foxneZz <foxnezz@gmail.com>
2017-03-19 14:07:18 +0300

    added more command line argument options

--- BookmarkStats/Program.cs
+++ BookmarkStats/Program.cs
@@ -8,10 +8,28 @@ namespace BookmarkStats
     {
         public static void Main(string[] args)
         {
-            var outputPath = args[0] == "-o" ? args[1] : "stats.txt";
+            var outputPath = "stats.txt";
+            var inputPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"Google\Chrome\User Data\Default\Bookmarks");
 
-            var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"Google\Chrome\User Data\Default\Bookmarks");
-            var bookmarks = BookmarkParser.Parse(File.ReadAllText(path));
+            try
+            {
+                for (int i = 0; i < args.Length; ++i)
+                {
+                    switch (args[i])
+                    {
+                        case "-o": outputPath = args[++i]; break;
+                        case "-i": inputPath = args[++i]; break;
+                        default: throw new Exception(); // ArgumentException("Wrong argument format");
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Wrong argument format");
+                return;
+            }
+
+            var bookmarks = BookmarkParser.Parse(File.ReadAllText(inputPath));
             var stats = BookmarkStats.Get(bookmarks);
             
             File.WriteAllText(outputPath, stats);
